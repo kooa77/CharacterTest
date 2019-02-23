2dfebdd6e155a6a25c16dfe9032457146d216167
Azu87 <duqrlehs@gmail.com>
2019-02-23 10:21:20 +0900

    강의 1

---
+++ Unity/Assets/01. Scripts/Character.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    // CharacterController를 _animator 에 세팅
+    [SerializeField] Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IdleState();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // 상태
+
+    void IdleState()
+    {
+        string triggerName = "idle1";
+        int randValue = Random.Range(1, 6);
+        triggerName = "idle" + randValue;
+        _animator.SetTrigger(triggerName);
+    }
+}
