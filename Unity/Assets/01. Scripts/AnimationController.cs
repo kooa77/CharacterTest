cc6ac167d5a8b6f1c783809966e90b778be36881
Azu87 <duqrlehs@gmail.com>
2019-02-23 12:48:05 +0900

    AnimationController.cs 파일 추가

---
+++ Unity/Assets/01. Scripts/AnimationController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    public Character _character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Animation Event
+
+    void AnimationEnd()
+    {
+        Debug.Log("AnimationEnd");
+
+        switch(_character.GetState())
+        {
+            case Character.eState.IDLE:
+                _character.ChangeState(Character.eState.WAIT);
+                break;
+            case Character.eState.WAIT:
+                _character.ChangeState(Character.eState.KICK);
+                break;
+            case Character.eState.KICK:
+                _character.ChangeState(Character.eState.IDLE);
+                break;
+        }
+    }
+}
